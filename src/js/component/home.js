72b7d00503b7345083a24c553a0ccc04495f8b26
Kty <katiuska.rodriguez5@gmail.com>
2021-05-11 17:39:43 +0000

    counter

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,18 +7,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
 		</div>
 	);
 }
