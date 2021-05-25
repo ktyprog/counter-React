3a903826660bc189216394336da94e2135d1574d
Kty <katiuska.rodriguez5@gmail.com>
2021-05-25 10:51:54 +0000

    contador sin style

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,18 +1,32 @@
+//import react al bundle
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import SecondsCounter from "./SecondsCounter";
 
 //create your first component
-export function Home() {
-	return;
-	<div className="text-center mt-5">
-		<div className="square"></div>
-		<div className="square"></div>
-		<div className="square"></div>
-		<div className="square"></div>
-		<div className="square"></div>
-		<div className="square"></div>
-		<div className="square"></div>
-	</div>;
+// export function Home() {
+// 	return (
+// 		<div className=" container- fluid text-center mt-5">
+// 			<SecondsCounter seconds="20" minutes="20" hour="25" />
+// 		</div>
+// 	);
+// }
+
+function Home({ number }) {
+	let arrayNumber = Array.from(String(number), Number);
+	return (
+		<div>
+			<h1>
+				{(arrayNumber[1], arrayNumber[0])}
+
+				{/* <div className="Clock">
+					<i className="far fa-clock" />
+				</div>
+				{numbersArray.map((num, i) => (
+					<div key={i}> {num}</div>
+				))} */}
+			</h1>
+		</div>
+	);
 }
+
+export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,11 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import Home from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let number = 0;
+setInterval(() => {
+	ReactDOM.render(<Home number={number} />, document.querySelector("#app"));
+	number += 1;
+}, 1000);
